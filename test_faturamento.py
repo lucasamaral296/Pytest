6fb82e6980df9a59c7bfc96ac15e9756f53aa671
Lucas Amaral Velasque <81877529+lucasamaral296@users.noreply.github.com>
2023-07-23 23:19:28 -0300

    criando marcadores

--- test_faturamento.py
+++ test_faturamento.py
@@ -1,4 +1,7 @@
 from main import calcular_faturamento
+import pytest
 
+@pytest.mark.faturamento
+# faturamento
 def test_calcular_faturamento():
     assert type(calcular_faturamento()) == int
--- test_main.py
+++ test_main.py
@@ -1,11 +1,14 @@
 from main import calcular_lucro, calcular_faturamento
+import pytest
 
 def test_calcular_lucro():
     faturamento = calcular_faturamento()
     assert calcular_lucro(faturamento, 500) > 0
     
-
-
+# faturamento
+@pytest.mark.faturamento
+def test_calcular_faturamento():
+    assert calcular_faturamento() > 0
 
 
 
@@ -16,3 +19,6 @@ def test_calcular_lucro():
 
 # pytest -v   DÁ MAIS INFORMAÇÕES DO TESTE
 # pytest arquivo.py testa apenas o arquivo 
+
+
+# marcadores, categoriza os testes. no terminal usar pytest -m nomedomarcador 
